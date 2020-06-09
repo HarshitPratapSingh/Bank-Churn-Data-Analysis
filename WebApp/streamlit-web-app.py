4205692ad9c6da3ab76fac586482e52389f08cef
Harshit Pratap Singh <4harshitsingh@gmail.com>
2020-06-09 20:48:43 +0530

    web-app

---
+++ WebApp/streamlit-web-app.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import pandas as pd
+
+
+@st.cache
+def get_data():
+    url = "https://raw.githubusercontent.com/HarshitPratapSingh/Bank-Churn-Data-Analysis/master/dataset/churn.csv"
+    return pd.read_csv(url)
+
+df = get_data()
+
+
+is_check = st.checkbox("Display Data")
+if is_check:
+    st.write(df)
+
+countries = st.sidebar.multiselect("Enter Countries",df['Geography'].unique())
+st.write("Your input countries", countries)
+
+variables = st.sidebar.multiselect("Select the columns", df.columns)
+st.write(variables)
