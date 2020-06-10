446c8fd584911dc75f7586b1ae137061afa99786
Harshit Pratap Singh <4harshitsingh@gmail.com>
2020-06-11 00:48:28 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,8 @@
-mkdir -p ~/.streamlit/
-
-echo "\
-[general]\n\
-email = \"4harshitsingh@gmail.com\"\n\
-" > ~/.streamlit/credentials.toml
+sh mkdir -p ~/.streamlit/
 
 echo "\
 [server]\n\
 headless = true\n\
-enableCORS=true\n\
 port = $PORT\n\
+enableCORS=false\n\
 " > ~/.streamlit/config.toml
